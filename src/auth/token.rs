77ce83f8b101762c8fb794857e2efc4e50f970c1
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-07-06 10:46:04 +0700

    Modify token procedures

--- src/auth/token.rs
+++ src/auth/token.rs
@@ -3,8 +3,9 @@ use chrono::{Utc, TimeZone};
 use rmcs_auth_db::Auth;
 use rmcs_auth_api::token::token_service_server::TokenService;
 use rmcs_auth_api::token::{
-    TokenSchema, RefreshId, AccessId, UserId, TokenUpdate,
-    TokenReadResponse, TokenListResponse, TokenCreateResponse, TokenUpdateResponse, TokenChangeResponse
+    TokenSchema, AuthToken, AccessId, UserId, AuthTokenCreate, TokenUpdate,
+    TokenReadResponse, TokenListResponse, TokenCreateResponse, AuthTokenCreateResponse, 
+    TokenUpdateResponse, TokenChangeResponse
 };
 
 pub struct TokenServer {
@@ -39,16 +40,16 @@ impl TokenService for TokenServer {
         Ok(Response::new(TokenReadResponse { result }))
     }
 
-    async fn read_refresh_token(&self, request: Request<RefreshId>)
-        -> Result<Response<TokenReadResponse>, Status>
+    async fn list_auth_token(&self, request: Request<AuthToken>)
+        -> Result<Response<TokenListResponse>, Status>
     {
         let request = request.into_inner();
-        let result = self.auth_db.read_refresh_token(&request.refresh_id).await;
-        let result = match result {
-            Ok(value) => Some(value.into()),
+        let result = self.auth_db.list_auth_token(&request.auth_token).await;
+        let results = match result {
+            Ok(value) => value.into_iter().map(|e| e.into()).collect(),
             Err(_) => return Err(Status::not_found(TOKEN_NOT_FOUND))
         };
-        Ok(Response::new(TokenReadResponse { result }))
+        Ok(Response::new(TokenListResponse { results }))
     }
 
     async fn list_token_by_user(&self, request: Request<UserId>)
@@ -69,31 +70,37 @@ impl TokenService for TokenServer {
         let request = request.into_inner();
         let result = self.auth_db.create_access_token(
             request.user_id,
+            &request.auth_token,
             Utc.timestamp_nanos(request.expire),
             request.ip.as_slice()
         ).await;
-        let (access_id, refresh_id) = match result {
+        let (access_id, refresh_token, auth_token) = match result {
             Ok(value) => value,
             Err(_) => return Err(Status::internal(TOKEN_CREATE_ERR))
         };
-        Ok(Response::new(TokenCreateResponse { access_id, refresh_id }))
+        Ok(Response::new(TokenCreateResponse { access_id, refresh_token, auth_token }))
     }
 
-    async fn create_refresh_token(&self, request: Request<TokenSchema>)
-        -> Result<Response<TokenCreateResponse>, Status>
+    async fn create_auth_token(&self, request: Request<AuthTokenCreate>)
+        -> Result<Response<AuthTokenCreateResponse>, Status>
     {
         let request = request.into_inner();
-        let result = self.auth_db.create_refresh_token(
-            request.access_id,
+        let result = self.auth_db.create_auth_token(
             request.user_id,
             Utc.timestamp_nanos(request.expire),
-            request.ip.as_slice()
+            request.ip.as_slice(),
+            request.number
         ).await;
-        let (access_id, refresh_id) = match result {
-            Ok(value) => value,
+        let tokens = match result {
+            Ok(value) => value.into_iter()
+                .map(|t| TokenCreateResponse {
+                    access_id: t.0,
+                    refresh_token: t.1,
+                    auth_token: t.2
+                }).collect(),
             Err(_) => return Err(Status::internal(TOKEN_CREATE_ERR))
         };
-        Ok(Response::new(TokenCreateResponse { access_id, refresh_id }))
+        Ok(Response::new(AuthTokenCreateResponse { tokens }))
     }
 
     async fn update_access_token(&self, request: Request<TokenUpdate>)
@@ -105,28 +112,27 @@ impl TokenService for TokenServer {
             request.expire.map(|s| Utc.timestamp_nanos(s)),
             request.ip.as_deref()
         ).await;
-        let refresh_id = match result {
+        let (refresh_token, auth_token) = match result {
             Ok(value) => value,
             Err(_) => return Err(Status::internal(TOKEN_UPDATE_ERR))
         };
-        Ok(Response::new(TokenUpdateResponse { refresh_id }))
+        Ok(Response::new(TokenUpdateResponse { refresh_token, auth_token }))
     }
 
-    async fn update_refresh_token( &self, request: Request<TokenUpdate>)
+    async fn update_auth_token( &self, request: Request<TokenUpdate>)
         -> Result<Response<TokenUpdateResponse>, Status>
     {
         let request = request.into_inner();
-        let result = self.auth_db.update_refresh_token(
-            request.refresh_id.unwrap_or_default().as_ref(),
-            request.access_id,
+        let result = self.auth_db.update_auth_token(
+            request.auth_token.unwrap_or_default().as_ref(),
             request.expire.map(|s| Utc.timestamp_nanos(s)),
             request.ip.as_deref()
         ).await;
-        let refresh_id = match result {
+        let (refresh_token, auth_token) = match result {
             Ok(value) => value,
             Err(_) => return Err(Status::internal(TOKEN_UPDATE_ERR))
         };
-        Ok(Response::new(TokenUpdateResponse { refresh_id }))
+        Ok(Response::new(TokenUpdateResponse { refresh_token, auth_token }))
     }
 
     async fn delete_access_token(&self, request: Request<AccessId>)
@@ -141,11 +147,11 @@ impl TokenService for TokenServer {
         Ok(Response::new(TokenChangeResponse { }))
     }
 
-    async fn delete_refresh_token(&self, request: Request<RefreshId>)
+    async fn delete_auth_token(&self, request: Request<AuthToken>)
         -> Result<Response<TokenChangeResponse>, Status>
     {
         let request = request.into_inner();
-        let result = self.auth_db.delete_refresh_token(&request.refresh_id).await;
+        let result = self.auth_db.delete_auth_token(&request.auth_token).await;
         match result {
             Ok(_) => (),
             Err(_) => return Err(Status::internal(TOKEN_DELETE_ERR))
