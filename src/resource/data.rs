0db40bdb86d9e09ac4b4e5038f6571bb68827415
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-08-23 21:14:23 +0700

    Remove list data by timestamp method

--- src/resource/data.rs
+++ src/resource/data.rs
@@ -54,23 +54,6 @@ impl DataService for DataServer {
         Ok(Response::new(DataReadResponse { result }))
     }
 
-    async fn list_data_by_time(&self, request: Request<DataTime>)
-        -> Result<Response<DataListResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.list_data_by_time(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp * 1000),
-        ).await;
-        let results = match result {
-            Ok(value) => value.into_iter().map(|e| e.into()).collect(),
-            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
-        };
-        Ok(Response::new(DataListResponse { results }))
-    }
-
     async fn list_data_by_last_time(&self, request: Request<DataTime>)
         -> Result<Response<DataListResponse>, Status>
     {
@@ -140,149 +123,6 @@ impl DataService for DataServer {
             Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
         };
         Ok(Response::new(DataListResponse { results }))
-    }
-
-    async fn read_data_timestamp(&self, request: Request<DataId>)
-        -> Result<Response<TimestampReadResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.read_data_timestamp(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp * 1000)
-        ).await;
-        let timestamp = match result {
-            Ok(value) => value.timestamp_micros(),
-            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
-        };
-        Ok(Response::new(TimestampReadResponse { timestamp }))
-    }
-
-    async fn list_data_timestamp_by_last_time(&self, request: Request<DataTime>)
-        -> Result<Response<TimestampListResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.list_data_timestamp_by_last_time(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp * 1000),
-        ).await;
-        let timestamps = match result {
-            Ok(value) => value.into_iter().map(|t| t.timestamp_micros()).collect(),
-            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
-        };
-        Ok(Response::new(TimestampListResponse { timestamps }))
-    }
-
-    async fn list_data_timestamp_by_range_time(&self, request: Request<DataRange>)
-        -> Result<Response<TimestampListResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.list_data_timestamp_by_range_time(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.begin * 1000),
-            Utc.timestamp_nanos(request.end * 1000)
-        ).await;
-        let timestamps = match result {
-            Ok(value) => value.into_iter().map(|t| t.timestamp_micros()).collect(),
-            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
-        };
-        Ok(Response::new(TimestampListResponse { timestamps }))
-    }
-
-    async fn create_data(&self, request: Request<DataSchema>)
-        -> Result<Response<DataChangeResponse>, Status>
-    {
-        self.validate(request.extensions(), CREATE_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.create_data(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp * 1000),
-            ArrayDataValue::from_bytes(
-                &request.data_bytes,
-                request.data_type.into_iter().map(|e| {
-                    DataType::from(common::DataType::try_from(e).unwrap_or_default())
-                }).collect::<Vec<DataType>>().as_slice()
-            ).to_vec()
-        ).await;
-        match result {
-            Ok(_) => (),
-            Err(_) => return Err(Status::internal(DATA_CREATE_ERR))
-        };
-        Ok(Response::new(DataChangeResponse { }))
-    }
-
-    async fn delete_data(&self, request: Request<DataId>)
-        -> Result<Response<DataChangeResponse>, Status>
-    {
-        self.validate(request.extensions(), DELETE_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.delete_data(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp * 1000),
-        ).await;
-        match result {
-            Ok(_) => (),
-            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
-        };
-        Ok(Response::new(DataChangeResponse { }))
-    }
-
-    async fn count_data(&self, request: Request<DataCount>)
-        -> Result<Response<DataCountResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.count_data(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default()
-        ).await;
-        let count = match result {
-            Ok(value) => value as u32,
-            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
-        };
-        Ok(Response::new(DataCountResponse { count }))
-    }
-
-    async fn count_data_by_last_time(&self, request: Request<DataCount>)
-        -> Result<Response<DataCountResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.count_data_by_last_time(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp.unwrap_or_default() * 1000)
-        ).await;
-        let count = match result {
-            Ok(value) => value as u32,
-            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
-        };
-        Ok(Response::new(DataCountResponse { count }))
-    }
-
-    async fn count_data_by_range_time(&self, request: Request<DataCount>)
-        -> Result<Response<DataCountResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.count_data_by_range_time(
-            Uuid::from_slice(&request.device_id).unwrap_or_default(),
-            Uuid::from_slice(&request.model_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.begin.unwrap_or_default() * 1000),
-            Utc.timestamp_nanos(request.end.unwrap_or_default() * 1000)
-        ).await;
-        let count = match result {
-            Ok(value) => value as u32,
-            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
-        };
-        Ok(Response::new(DataCountResponse { count }))
     }
 
     async fn list_data_by_set_time(&self, request: Request<DataSetTime>)
@@ -384,22 +224,6 @@ impl DataService for DataServer {
         Ok(Response::new(DataSetReadResponse { result }))
     }
 
-    async fn list_data_set_by_time(&self, request: Request<DataSetTime>)
-        -> Result<Response<DataSetListResponse>, Status>
-    {
-        self.validate(request.extensions(), READ_DATA)?;
-        let request = request.into_inner();
-        let result = self.resource_db.list_data_set_by_time(
-            Uuid::from_slice(&request.set_id).unwrap_or_default(),
-            Utc.timestamp_nanos(request.timestamp * 1000),
-        ).await;
-        let results = match result {
-            Ok(value) => value.into_iter().map(|e| e.into()).collect(),
-            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
-        };
-        Ok(Response::new(DataSetListResponse { results }))
-    }
-
     async fn list_data_set_by_last_time(&self, request: Request<DataSetTime>)
         -> Result<Response<DataSetListResponse>, Status>
     {
@@ -467,12 +291,104 @@ impl DataService for DataServer {
         Ok(Response::new(DataSetListResponse { results }))
     }
 
-    async fn read_data_set_timestamp(&self, request: Request<DataSetId>)
+    async fn create_data(&self, request: Request<DataSchema>)
+        -> Result<Response<DataChangeResponse>, Status>
+    {
+        self.validate(request.extensions(), CREATE_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.create_data(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.timestamp * 1000),
+            ArrayDataValue::from_bytes(
+                &request.data_bytes,
+                request.data_type.into_iter().map(|e| {
+                    DataType::from(common::DataType::try_from(e).unwrap_or_default())
+                }).collect::<Vec<DataType>>().as_slice()
+            ).to_vec()
+        ).await;
+        match result {
+            Ok(_) => (),
+            Err(_) => return Err(Status::internal(DATA_CREATE_ERR))
+        };
+        Ok(Response::new(DataChangeResponse { }))
+    }
+
+    async fn delete_data(&self, request: Request<DataId>)
+        -> Result<Response<DataChangeResponse>, Status>
+    {
+        self.validate(request.extensions(), DELETE_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.delete_data(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.timestamp * 1000),
+        ).await;
+        match result {
+            Ok(_) => (),
+            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
+        };
+        Ok(Response::new(DataChangeResponse { }))
+    }
+
+    async fn read_data_timestamp(&self, request: Request<DataId>)
         -> Result<Response<TimestampReadResponse>, Status>
     {
         self.validate(request.extensions(), READ_DATA)?;
         let request = request.into_inner();
-        let result = self.resource_db.read_data_set_timestamp(
+        let result = self.resource_db.read_data_timestamp(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.timestamp * 1000)
+        ).await;
+        let timestamp = match result {
+            Ok(value) => value.timestamp_micros(),
+            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
+        };
+        Ok(Response::new(TimestampReadResponse { timestamp }))
+    }
+
+    async fn list_data_timestamp_by_last_time(&self, request: Request<DataTime>)
+        -> Result<Response<TimestampListResponse>, Status>
+    {
+        self.validate(request.extensions(), READ_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.list_data_timestamp_by_last_time(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.timestamp * 1000),
+        ).await;
+        let timestamps = match result {
+            Ok(value) => value.into_iter().map(|t| t.timestamp_micros()).collect(),
+            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
+        };
+        Ok(Response::new(TimestampListResponse { timestamps }))
+    }
+
+    async fn list_data_timestamp_by_range_time(&self, request: Request<DataRange>)
+        -> Result<Response<TimestampListResponse>, Status>
+    {
+        self.validate(request.extensions(), READ_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.list_data_timestamp_by_range_time(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.begin * 1000),
+            Utc.timestamp_nanos(request.end * 1000)
+        ).await;
+        let timestamps = match result {
+            Ok(value) => value.into_iter().map(|t| t.timestamp_micros()).collect(),
+            Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
+        };
+        Ok(Response::new(TimestampListResponse { timestamps }))
+    }
+
+    async fn read_data_timestamp_by_set(&self, request: Request<DataSetId>)
+        -> Result<Response<TimestampReadResponse>, Status>
+    {
+        self.validate(request.extensions(), READ_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.read_data_timestamp_by_set(
             Uuid::from_slice(&request.set_id).unwrap_or_default(),
             Utc.timestamp_nanos(request.timestamp * 1000)
         ).await;
@@ -483,12 +399,12 @@ impl DataService for DataServer {
         Ok(Response::new(TimestampReadResponse { timestamp }))
     }
 
-    async fn list_data_set_timestamp_by_last_time(&self, request: Request<DataSetTime>)
+    async fn list_data_timestamp_by_set_last_time(&self, request: Request<DataSetTime>)
         -> Result<Response<TimestampListResponse>, Status>
     {
         self.validate(request.extensions(), READ_DATA)?;
         let request = request.into_inner();
-        let result = self.resource_db.list_data_set_timestamp_by_last_time(
+        let result = self.resource_db.list_data_timestamp_by_set_last_time(
             Uuid::from_slice(&request.set_id).unwrap_or_default(),
             Utc.timestamp_nanos(request.timestamp * 1000),
         ).await;
@@ -499,12 +415,12 @@ impl DataService for DataServer {
         Ok(Response::new(TimestampListResponse { timestamps }))
     }
 
-    async fn list_data_set_timestamp_by_range_time(&self, request: Request<DataSetRange>)
+    async fn list_data_timestamp_by_set_range_time(&self, request: Request<DataSetRange>)
         -> Result<Response<TimestampListResponse>, Status>
     {
         self.validate(request.extensions(), READ_DATA)?;
         let request = request.into_inner();
-        let result = self.resource_db.list_data_set_timestamp_by_range_time(
+        let result = self.resource_db.list_data_timestamp_by_set_range_time(
             Uuid::from_slice(&request.set_id).unwrap_or_default(),
             Utc.timestamp_nanos(request.begin * 1000),
             Utc.timestamp_nanos(request.end * 1000)
@@ -514,6 +430,57 @@ impl DataService for DataServer {
             Err(_) => return Err(Status::not_found(DATA_NOT_FOUND))
         };
         Ok(Response::new(TimestampListResponse { timestamps }))
+    }
+
+    async fn count_data(&self, request: Request<DataCount>)
+        -> Result<Response<DataCountResponse>, Status>
+    {
+        self.validate(request.extensions(), READ_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.count_data(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default()
+        ).await;
+        let count = match result {
+            Ok(value) => value as u32,
+            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
+        };
+        Ok(Response::new(DataCountResponse { count }))
+    }
+
+    async fn count_data_by_last_time(&self, request: Request<DataCount>)
+        -> Result<Response<DataCountResponse>, Status>
+    {
+        self.validate(request.extensions(), READ_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.count_data_by_last_time(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.timestamp.unwrap_or_default() * 1000)
+        ).await;
+        let count = match result {
+            Ok(value) => value as u32,
+            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
+        };
+        Ok(Response::new(DataCountResponse { count }))
+    }
+
+    async fn count_data_by_range_time(&self, request: Request<DataCount>)
+        -> Result<Response<DataCountResponse>, Status>
+    {
+        self.validate(request.extensions(), READ_DATA)?;
+        let request = request.into_inner();
+        let result = self.resource_db.count_data_by_range_time(
+            Uuid::from_slice(&request.device_id).unwrap_or_default(),
+            Uuid::from_slice(&request.model_id).unwrap_or_default(),
+            Utc.timestamp_nanos(request.begin.unwrap_or_default() * 1000),
+            Utc.timestamp_nanos(request.end.unwrap_or_default() * 1000)
+        ).await;
+        let count = match result {
+            Ok(value) => value as u32,
+            Err(_) => return Err(Status::internal(DATA_DELETE_ERR))
+        };
+        Ok(Response::new(DataCountResponse { count }))
     }
 
 }
